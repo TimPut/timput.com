ad9bcc0c88ef1daf68da85d672ee9d808260d770
timput <tim@timput.com>
2022-08-06 14:25:54 -0600

    messy mixed commit

--- publi.sh
+++ publi.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nix-build && rsync -P -vr -e ssh -i /home/tim/.ssh/general result/ tput_put@ssh.phx.nearlyfreespeech.net:/home/public/
+nix-build --builders '' && rsync -P -vr -e ssh -i /home/tim/.ssh/general result/ tput_put@ssh.phx.nearlyfreespeech.net:/home/public/
